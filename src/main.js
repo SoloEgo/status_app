add8a9c81c51877cdb1ffd8c09da8bd80c291cb1
Egor <solovev_ea@metholding.com>
2022-12-12 22:31:38 +0300

    3

--- src/main.js
+++ src/main.js
@@ -6,21 +6,21 @@ import store from './store'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getMessaging, getToken  } from "firebase/messaging";
+import { getMessaging, getToken } from "firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC4keGX5MRyVTYoX7NMQtHMRujg2p3TmVU",
-  authDomain: "server-status-app-5b30d.firebaseapp.com",
-  databaseURL: "https://server-status-app-5b30d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "server-status-app-5b30d",
-  storageBucket: "server-status-app-5b30d.appspot.com",
-  messagingSenderId: "369043395334",
-  appId: "1:369043395334:web:ec7eeb787d1993b35e0487",
-  measurementId: "G-0CN7X3408F"
+    apiKey: "AIzaSyC4keGX5MRyVTYoX7NMQtHMRujg2p3TmVU",
+    authDomain: "server-status-app-5b30d.firebaseapp.com",
+    databaseURL: "https://server-status-app-5b30d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "server-status-app-5b30d",
+    storageBucket: "server-status-app-5b30d.appspot.com",
+    messagingSenderId: "369043395334",
+    appId: "1:369043395334:web:ec7eeb787d1993b35e0487",
+    measurementId: "G-0CN7X3408F"
 };
 
 // Initialize Firebase
@@ -30,16 +30,20 @@ const analytics = getAnalytics(app);
 createApp(App).use(store).use(router).mount('#app')
 
 const messaging = getMessaging(app);
-getToken(messaging, {vapidKey: "BIbMQKF9D97qH5z5pS0WupSADTFBzy3Pu-Kw1XQ-X9kg17u9shRAHo-S8r6uPWNuxfYT-fRxa21kp5kKEF63owY"});
+getToken(messaging, { vapidKey: "BIbMQKF9D97qH5z5pS0WupSADTFBzy3Pu-Kw1XQ-X9kg17u9shRAHo-S8r6uPWNuxfYT-fRxa21kp5kKEF63owY" });
 
 // Request Permission of Notifications
-messaging.requestPermission().then(() => {
-    console.log('Notification permission granted.');
-    
-    // Get Token
-    messaging.getToken().then((token) => {
-        console.log(token)
-    })
+getToken(messaging, { vapidKey: '<YOUR_PUBLIC_VAPID_KEY_HERE>' }).then((currentToken) => {
+    if (currentToken) {
+        console.log(currentToken)
+        // Send the token to your server and update the UI if necessary
+        // ...
+    } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+        // ...
+    }
 }).catch((err) => {
-    console.log('Unable to get permission to notify.', err);
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
 });
